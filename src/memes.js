55fbd846a56c9652e03cd56ae9829f71af569fc1
saptarshi <saptarshichattopadhyay543@gmail.com>
2022-01-08 15:58:46 +0530

    error in exporting Memes class

--- src/memes.js
+++ src/memes.js
@@ -9,6 +9,7 @@ class Memes extends React.Component{
             width: "300px",
             height:"200px"
     };
+	    {/*error in exporting ....*/}
         return(
             <div>
                  <img style={mystyle} alt="just some memes" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic1.srcdn.com%2Fwordpress%2Fwp-content%2Fuploads%2F2020%2F04%2FLuke-Skywalker-Memes-Featured.jpg&f=1&nofb=1" />
