9a976d245920574dee3f77eea5f57862291b30ec
saptarshi <saptarshichattopadhyay543@gmail.com>
2022-01-13 13:02:39 +0530

    reverted because of problems

--- src/Show2.js
+++ src/Show2.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import SimpleImageSlider from 'react-simple-image-slider';
-
 // const images=[
 //   {url: "./download.jpeg"},
 //   {url: "./pic1.jpeg"},
@@ -11,15 +10,20 @@ import SimpleImageSlider from 'react-simple-image-slider';
 // ];
 
 const images=[
-  {url:"https://popcornerreviews.com/wp-content/uploads/2020/08/funny-qui-gon-star-wars-meme.jpg"},
- { url:"https://popcornerreviews.com/wp-content/uploads/2020/08/anakin-star-wars-prequel-meme.jpg"},
+  {url:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic1.srcdn.com%2Fwordpress%2Fwp-content%2Fuploads%2F2020%2F04%2FLuke-Skywalker-Memes-Featured.jpg&f=1&nofb=1"},
+ { url:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffilmgoblin.com%2Fwp-content%2Fuploads%2F2019%2F04%2Fanh-2.jpg&f=1&nofb=1"},
+ {url: "https://popcornerreviews.com/wp-content/uploads/2020/08/funny-qui-gon-star-wars-meme.jpg"},
+ {url: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ftheawesomedaily.com%2Fwp-content%2Fuploads%2F2016%2F12%2Fstar-wars-memes-4-1.jpg&f=1&nofb=1"},
+ {url: "https://popcornerreviews.com/wp-content/uploads/2020/08/anakin-star-wars-prequel-meme.jpg"}
 ];
+
 const myStyle={
   marginLeft:"25%"
 };
 const Show=()=>{
   return(
     <div>
+    <Box m={2} pt={2}>
       <SimpleImageSlider 
       style={myStyle}
       width={896}
@@ -27,6 +31,7 @@ const Show=()=>{
       images={images}
       showBullets={true}
       showNavs={true} />
+      
     </div>
   );
 }
