3907faf81de75e7d707405e185a5afe9a5fa04b0
saptarshi <saptarshichattopadhyay543@gmail.com>
2022-01-19 13:07:28 +0530

    tried to install bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
